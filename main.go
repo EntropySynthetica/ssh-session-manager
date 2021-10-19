e3b587949bcc66d56f0bfa30f5c90a691ae8a4af
Erica Zavaleta <44880722+EntropySynthetica@users.noreply.github.com>
2021-10-19 17:18:41 -0500

    Hostname now shows in selection menu

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ type Host struct {
 
 func main() {
 
-	// Open the inventory file
+	// Open the hosts file
 	jsonFile, err := os.Open("hosts.json")
 
 	if err != nil {
@@ -32,37 +32,29 @@ func main() {
 
 	defer jsonFile.Close()
 
-	// Unmarshal the JSON from the inventory file
+	// Unmarshal the JSON from the file
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 
 	var host Hosts
 	json.Unmarshal(byteValue, &host)
 
-	//// Debug for loop to make sure the JSON file is getting parsed.
-	// for i := 0; i < len(host.Hosts); i++ {
-	// 	fmt.Println("Hostname: " + host.Hosts[i].Name)
-	// 	fmt.Println("IP: " + host.Hosts[i].Ip)
-	// 	fmt.Println("User: " + host.Hosts[i].User)
-	// 	fmt.Println("Group: " + host.Hosts[i].Group)
-	// }
-
 	// Create emtpy array and dump the IPs into it.
 	devices := []string{}
 	for i := 0; i < len(host.Hosts); i++ {
-		devices = append(devices, host.Hosts[i].Ip)
+		devices = append(devices, host.Hosts[i].Name)
 	}
 
 	// Bring up the menu for the user to select the device to connect to.
-	ssh_hostname := ""
+	var selectDevice int
 	prompt := &survey.Select{
 		Message: "Select a device to connect to:",
 		Options: devices,
 	}
-	survey.AskOne(prompt, &ssh_hostname)
+	survey.AskOne(prompt, &selectDevice, survey.WithPageSize(10))
 
 	// Run SSH and pass it the hostname of the device to connect to.
-	fmt.Println("Connecting to " + ssh_hostname)
-	sshArgs := "ssh erica@" + ssh_hostname
+	fmt.Println("Connecting to " + host.Hosts[selectDevice].Name)
+	sshArgs := "ssh " + host.Hosts[selectDevice].User + "@" + host.Hosts[selectDevice].Ip
 	cmd := exec.Command("bash", "-c", sshArgs)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
